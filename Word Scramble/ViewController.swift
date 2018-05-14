a1afd7ac877fdfcaabf26af73346b4c95cdc09f8
nikitaseliverstov <nikita77077@inbox.ru>
2018-05-14 23:43:04 +0300

    Initial Commit

---
+++ Word Scramble/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Word Scramble
+//
+//  Created by Nikita Seliverstov  on 14.05.2018.
+//  Copyright © 2018 Nikita Seliverstov . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
